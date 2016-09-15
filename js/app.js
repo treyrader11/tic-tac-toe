29473e06a7ea71110346a00612f9ca6c9f241300
Trey Rader <treyrdr09@gmail.com>
2016-09-15 18:52:10 -0500

    restart game

--- js/app.js
+++ js/app.js
@@ -8,9 +8,7 @@ $(document).ready(function() {
 		$('#begin-game').toggle('explode', function() {
 			$(this).remove();
 			$('#game').fadeIn(500, function() {
-
 				beginGame();
-				
 			});
 		}) ;
 		$('#bg-img').toggle('explode', function() {
@@ -20,10 +18,9 @@ $(document).ready(function() {
 })
 
 
-$(document).ready(function() {
-	//var x = 'x';
-	//var o = 'o';
+function beginGame() {
 	var turn = 0;
+	console.log('new game', turn)
 	//Spot vars
 	var spot1 = $('#spot1');
 	var spot2 = $('#spot2');
@@ -35,42 +32,33 @@ $(document).ready(function() {
 	var spot8 = $('#spot8');
 	var spot9 = $('#spot9');
 	
-	var player1 = prompt('Type in the name of player 1');
-	var player2 = prompt('Type in the name of player 2');
+	var player1 = prompt('Enter the name for player 1');
+	var player2 = prompt('Enter the name for player 2');
 	var x = "<li><h2>" +player1+ ", you're up!</h2></li>";
 	var o = "<li><h2>" +player2+ ", you're up!</h2></li>";
+	$('#results-list').find('li').remove();
+	console.log("player1 is " +player1);
+	console.log("player2 is " +player2);
+	$('#players').append(x);
+	turn++;	
 
 	
-
-	/*$('#begin-game').fadeIn(1000);
-	$('#bg-img').fadeIn(1000);
-
-	$('#yes').on("click", function() {
-		$('#begin-game').toggle('explode', function() {
-			$(this).remove();
-			$('#game').fadeIn(500, function() {
-
-				beginGame();
-				//showPlayers();
-			});
-		}) ;
-		$('#bg-img').toggle('explode', function() {
-			$(this).remove();
-		});
-	})*/
 
 
 	$('.square').on('click', function() {
 		turn++;
+		$('#players').find('li').remove()
+		console.log("round " +turn);
 		if(turn % 2 == 0) {
 			$(this).addClass('o').text("o");
+			//$('#players').find('li').remove();
 			$('#players').append(o);
 		}
 		 else {
-		 	$(this).toggleClass('x').text("x");
+		 	$(this).addClass('x').text("x");
+		 	//$('#players').find('li').remove();
 		 	$('#players').append(x);
 		 }
-		 //showPlayers(turn)
 	})
 
 
@@ -85,6 +73,10 @@ $(document).ready(function() {
 		spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
 		spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
 		spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o')) {
+			$('#players').find('li').remove()
+			$('#game').fadeOut(1000, function() {
+				$('#results-modal').fadeIn(1000)
+			})
 			showPlayer1(player1, player2, turn);
 
 		}
@@ -97,49 +89,67 @@ $(document).ready(function() {
 		spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
 		spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
 		spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x')) {
+			$('#players').find('li').remove();
+			$('#game').fadeOut(1000, function() {
+				$('#results-modal').fadeIn(1000)
+			})
 			showPlayer2(player2, player1, turn);
-			
 		}
 
 		else if(turn == 9) {
-			alert('Tie Game');
-			$('#board td').empty();
-			$('#board td').removeClass('o');
-			$('#board td').removeClass('x');
-			turn = 0;
+			$('#game').fadeOut(1000, function() {
+				$('#results-modal').fadeIn(1000)
+			})
+			showTie(turn);
 		}
-	})
-})
+	});
+};
 
-function beginGame() {
-	//var turn = 0;
-	//showPlayers(turn)
-	//console.log(turn)
-}
 
 function showPlayer1(player1, player2, turn) {
-	$('#board td').empty();
+	//turn = 0;
+	$('.square').empty();
 	$('#board td').removeClass('o');
 	$('#board td').removeClass('x');
 	var winner = "<li><h1>Congratulations, " +player1+ ". You wupped " +player2+ " in Tic Tac Toe!</h1></li>";
 	$('#results-list').append(winner);
-	turn = 0;
 	$('#game').fadeOut(1000, function() {
 		$('#results-modal').fadeIn(1000)
 	})
 }
 
 function showPlayer2(player2, player1, turn) {
+	//turn = 0;
 	$('#board td').empty();
 	$('#board td').removeClass('o');
 	$('#board td').removeClass('x');
 	var winner = "<li><h1>Congratulations, " +player2+ ". You wupped " +player1+ " in Tic Tac Toe!</h1></li>";
 	$('#results-list').append(winner);
-	turn = 0;
+	$('#game').fadeOut(1000, function() {
+		$('#results-modal').fadeIn(1000)
+	})
+
+
+}
+
+function showTie(turn) {
+	$('#board td').empty();
+	$('#board td').removeClass('o');
+	$('#board td').removeClass('x');
+	var tie = "<li><h1>Whoa, it's a tie! I guess thismeans that you two are true tic tac toe rivals!</h1></li>";
+	$('#results-list').append(tie);
 	$('#game').fadeOut(1000, function() {
 		$('#results-modal').fadeIn(1000)
 	})
 }
+
+$('#replay').on('click', function() {
+	$('#results-modal').fadeOut(500, function() {
+		$('#game').fadeIn(1000, function() {
+			beginGame();
+		});
+	})
+})
 
 
 

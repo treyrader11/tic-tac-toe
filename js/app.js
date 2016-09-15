96bb2081984668cb0df7752a8201b79be5f683e0
Trey Rader <treyrdr09@gmail.com>
2016-09-15 09:08:49 -0500

    stupid stuff

--- js/app.js
+++ js/app.js
@@ -2,9 +2,18 @@
 
 
 $(document).ready(function() {
+
 	$('#main-wrapper').find('.row').children().click(function() {
 		$(this).toggleClass('red');
 		//$( ".draggable" ).draggable();
+	})
+
+	$('#main-wrapper').mouseenter(function() {
+		$('#header-1').fadeOut(1000, function() {
+			$(this).remove();
+			$('#header-2').fadeIn(1000);
+		});
+		
 	})
 })
 
@@ -15,3 +24,4 @@ $( function() {
 
 /*all you need to do is include the jqueryUI script tag
 //and add draggable() function to the selector*/
+
